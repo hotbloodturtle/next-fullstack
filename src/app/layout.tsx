6ee72e02217b4df47837b420ff465cef35c5a3b8
Haeseok Kang <hotbloodturtle@gmail.com>
2023-02-22 19:28:10 +0900

    :sparkles: add - prisma

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,12 @@
-import './globals.css'
+import "./globals.css";
+import "./reset.css";
+import { ReactNode } from "react";
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
+    <html lang="ko-KO">
       <head />
       <body>{children}</body>
     </html>
-  )
+  );
 }
--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,13 +1,17 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
+import { prisma } from "@/lib/db";
+import { user } from ".prisma/client";
 
 type Data = {
-  name: string
-}
+  name: string;
+  users: user[];
+};
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  const users = await prisma.user.findMany();
+  res.status(200).json({ name: "John Doe", users });
 }
