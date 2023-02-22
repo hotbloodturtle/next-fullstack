0b1546c1777e516756adf4574a140f20214a80b8
Haeseok Kang <hotbloodturtle@gmail.com>
2023-02-22 19:39:35 +0900

    :sparkles: add - daisyui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,14 @@
 const Home = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <div className="badge">ERR</div>
+      <div className="flex justify-center">
+        <button className="btn">Button 1</button>
+        <button className="btn">Button 2</button>
+      </div>
+    </>
+  );
 };
 
 export default Home;
