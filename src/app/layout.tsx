b63af504f379c1f92ac69bc1b70a4322ccdab2b2
hotbloodturtle <bloodturtle@naver.com>
2023-02-25 14:25:34 +0900

    :fire: remove - react-query, unstated-next

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,13 @@
 import "./globals.css";
 import "./reset.css";
 import { ReactNode } from "react";
-import ReactQueryProvider from "@/containers/ReactQueryProvider";
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="ko-KR">
       <head />
       <body>
-        <ReactQueryProvider>{children}</ReactQueryProvider>
+        {children}
       </body>
     </html>
   );
